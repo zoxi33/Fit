cf35e7e1c5bd8bb6e09e234670099e656082cdbd
Michał <michalgrzejdak9@gmail.com>
2024-10-06 16:29:10 +0200

    asf

--- src/js/script.js
+++ src/js/script.js
@@ -2,6 +2,7 @@ const hamburgerBtn = document.querySelector('.hamburger-icon')
 const menuMobile = document.querySelector('.menu-mobile')
 const closeBtn = document.querySelector('.close-icon')
 const allLinks = menuMobile.querySelectorAll('a')
+
 const showNav = () => {
 	menuMobile.classList.toggle('show-nav')
 	allLinks.forEach(el => {
@@ -14,4 +15,17 @@ const showNav = () => {
 hamburgerBtn.addEventListener('click', showNav)
 closeBtn.addEventListener('click', () => {
 	menuMobile.classList.toggle('show-nav')
+})
+window.addEventListener('scroll', () => {
+	const navbar = document.querySelector('.navbar')
+	const navbarDesktop = document.querySelector('.navbar-dekstop')
+	if (window.scrollY > 50) {
+		// Wartość scrolla, po której dodasz tło
+
+		navbar.classList.add('scrolled')
+		navbarDesktop.classList.add('scrolled')
+	} else {
+		navbar.classList.remove('scrolled')
+		navbarDesktop.classList.remove('scrolled')
+	}
 })
